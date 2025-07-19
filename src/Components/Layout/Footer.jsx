6d5a90dfa3341a1fbc390c5e6ff8e03020e56859
mohitrajgupta01 <mohitraj46306@gmail.com>
2025-07-19 15:17:08 +0530

    Add Footer component

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <div className="container">
+          <p>&copy; 2025 Techinofyt. All Rights reserved</p>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
