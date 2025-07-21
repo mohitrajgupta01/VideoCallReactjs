38da9ab7a549e262aa2ec6b7ee22b811b1b99ace
mohitrajgupta01 <mohitraj46306@gmail.com>
2025-07-21 18:27:00 +0530

    Add Contact page component

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Layout from "../Components/Layout/Layout";
+
+const Contact = () => {
+  return (
+    <Layout>
+      <div
+        className="content"
+        style={{
+          height: "75vh",
+          margin: "30px",
+        }}
+      >
+        <h1>Contact US</h1>
+        <hr />
+        <h2>Phone : 0123456789</h2>
+        <h2>email : mohit46306@gmail.com</h2>
+        <h2>Instagram : mohitrajgupta01</h2>
+        
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
