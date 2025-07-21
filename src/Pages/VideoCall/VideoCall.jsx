ec5ae3b6aa592346e137a79041f0684cf7093924
mohitrajgupta01 <mohitraj46306@gmail.com>
2025-07-21 18:27:31 +0530

    Add VideoCall main component

---
+++ src/Pages/VideoCall/VideoCall.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import Layout from "../../Components/Layout/Layout";
+
+const VideoCall = () => {
+  const { id } = useParams();
+  const roomID = id;
+  let myMeeting = async (element) => {
+    // generate Kit Token
+    const appID = Number(import.meta.env.VITE_APP_ID);
+    const serverSecret = import.meta.env.VITE_APP_SECRET;
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      Date.now().toString(),
+      "mohit"
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: "share Call link",
+          url:
+            window.location.protocol +
+            "//" +
+            window.location.host +
+            window.location.pathname +
+            "?roomID=" +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+  return (
+    <>
+      <div
+        className="myCallContainer"
+        ref={myMeeting}
+        style={{ width: "100vw", height: "100vh" }}
+      ></div>
+    </>
+  );
+};
+
+export default VideoCall;
